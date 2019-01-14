b4833a35f30b9a3a306330e7799790cb1924b71f
Petr Nikolaev <petr.nikolaev@vismart.biz>
2019-01-14 11:09:19 +0300

    finish second lesson

--- select/script.js
+++ select/script.js
@@ -4,16 +4,10 @@ function selectFruit() {
     document.getElementsByClassName('output')[0].innerText = `Selected element ${options[select.selectedIndex].text}`;
 }
 
-function findExp() {
-    let unknownVar = 0;
-    const x = 0.017 * 5.4 * Math.pow(10, 11);
-    // for (let i = 0; i < 1; i+=0.001) {
-        const value = Math.exp(116298 / (8.31 * 450));
-    //     if (x > value - 0.5 && x < value + 0.5) {
-    //             console.log(i);
-    //         }
-    // }
-    console.log('nine');
+function rangeEvent() {
+    const range = document.getElementById('range-first');
+    const body = document.getElementsByTagName('body');
+    body[0].style.backgroundColor = `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`;
+    const rangeValue = document.getElementsByClassName('range-value');
+    rangeValue[0].innerText = range.value;
 }
-
-findExp();
