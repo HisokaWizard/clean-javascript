4caa9a543aab8c68cffe5a8227273f8caa77b155
Petr Nikolaev <petr.nikolaev@vismart.biz>
2019-01-18 16:07:53 +0300

    add lesson 6 modal window

---
+++ modal-window/script.js
@@ -0,0 +1,9 @@
+function openModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+}
