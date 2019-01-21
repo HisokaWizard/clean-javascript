0d38b76e9c32383044559432435b05d0016d71a1
Petr Nikolaev <petr.nikolaev@vismart.biz>
2019-01-21 18:53:00 +0300

    Finish lesson animation

--- animation/script.js
+++ animation/script.js
@@ -31,23 +31,68 @@
     };
 } */
 
+// function move() {
+//     const animation = document.getElementById('animation');
+//     const container = document.getElementById('container');
+//     let positionX = 0;
+//     let positionY = 0;
+//     let id;
+
+//     const frame = (finish) => {
+//         if (positionX === finish && positionY === finish / 2) {
+//             clearInterval(id);
+//         } else {
+//             positionX = Math.round(Math.random() * (container.clientWidth - animation.clientWidth));
+//             positionY = Math.round(Math.random() * (container.clientHeight - animation.clientHeight)); 
+//             animation.style.top = `${positionX}px`;
+//             animation.style.left = `${positionY}px`;
+//         }
+//     };
+
+//     id = setInterval(() => frame(50), 200);       
+// }
+
+let interval;
 function move() {
     const animation = document.getElementById('animation');
     const container = document.getElementById('container');
-    let positionX = 0;
-    let positionY = 0;
-    let id;
+    const minPosX = 0;
+    const minPosY = 0;
+    const maxPosX = container.clientWidth - animation.clientWidth;
+    const maxPosY = container.clientHeight - animation.clientHeight;
+    let posX = 0;
+    let posY = 0;
+    let stepX = 0;
+    let stepY = 0;
+    let directionX = true;
+    let directionY = true;
 
-    const frame = (finish) => {
-        if (positionX === finish && positionY === finish / 2) {
-            clearInterval(id);
-        } else {
-            positionX = Math.round(Math.random() * (container.clientWidth - animation.clientWidth));
-            positionY = Math.round(Math.random() * (container.clientHeight - animation.clientHeight)); 
-            animation.style.top = `${positionX}px`;
-            animation.style.left = `${positionY}px`;
+    const startMoving = () => {
+        if (posX <= minPosX || posX >= maxPosX || posY <= minPosY || posY >= maxPosY) {
+            stepX = Math.round(Math.random() * 5);
+            stepY = Math.round(Math.random() * 5);
+            if (posX <= minPosX) directionX = true;
+            if (posX >= maxPosX) directionX = false;
+            if (posY <= minPosY) directionY = true;
+            if (posY >= maxPosY) directionY = false;
         }
-    };
+        if (directionX) {
+            posX += stepX;
+        } else {
+            posX -= stepX;
+        }
+        if (directionY) {
+            posY += stepY;
+        } else {
+            posY -= stepY;
+        }
+        animation.style.left = `${posX}px`;
+        animation.style.top = `${posY}px`;
+    }
 
-    id = setInterval(() => frame(50), 200);       
+    interval = setInterval(startMoving, 10);
+}
+
+function stopMove() {
+    clearInterval(interval);
 }
