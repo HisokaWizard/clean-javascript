425fefacaad855cf4e851225714d399b7576016e
Petr Nikolaev <petr.nikolaev@vismart.biz>
2019-02-18 23:29:05 +0300

    added some functionality.

--- rxjs-started/src/from-event/fromEvent.js
+++ rxjs-started/src/from-event/fromEvent.js
@@ -2,7 +2,7 @@ import { fromEvent } from 'rxjs';
 import { scan, map } from 'rxjs/operators';
 
 export function screenClicker() {
-    const click = fromEvent(document.body, 'click');
+    const click = fromEvent(document.body, 'mousemove');
     click.pipe(
         map(event => {
             const coord = {
@@ -12,21 +12,38 @@ export function screenClicker() {
             return coord;
         }),
         scan((count, coord) => {
-            const value = ++count;
-            console.log(value);
             return {
-                coord: coord,
-                count: value
+                count: count,
+                coord: coord
             }
-        }, 0)
+        } , 0)
     )
     .subscribe(result => {
-        console.log(result);
+        createObject(result);
+        setTimeout(() => {
+            deleteObjects();
+        }, 100);
+    });
+
+    const createObject = (result) => {
         const text = document.createElement('div');
-        text.innerText = `Click count: ${result.count}`;
+        text.innerText = `x: ${result.coord.x}, y: ${result.coord.y}`;
         text.style.position = 'absolute';
         text.style.top = result.coord.y;
         text.style.left = result.coord.x;
+        text.style.border = '1px solid blue';
+        text.style.borderRadius = '10px';
+        text.style.width = '100px';
+        text.style.height = '20px';
+        text.style.backgroundColor = 'lightblue';
+        text.style.textAlign = 'center';
         document.body.appendChild(text);
-    });
+    };
+
+    const deleteObjects = () => {
+        const divElems = document.getElementsByTagName('div');
+        for (let index = 0; index < divElems.length; index++) {
+            document.body.removeChild(divElems[index]);
+        }
+    };
 }
